ba86cbdf06fcb71f5bfa29372416269e0414a9d8
Ahsan Ali <ahsanali274@gmail.com>
2024-07-22 18:34:27 +0500

    Updated map and pins

--- app/src/main/java/com/map/sampleapp/MainActivity.kt
+++ app/src/main/java/com/map/sampleapp/MainActivity.kt
@@ -3,12 +3,14 @@ package com.map.sampleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.LatLng
 import com.map.sampleapp.ui.CustomMapView
 import com.map.sampleapp.ui.theme.MyApplicationTheme
@@ -16,20 +18,28 @@ import com.map.sampleapp.ui.theme.MyApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         val list = listOf(
             MapPin("santiago", LatLng(-33.4489, -70.6693)),
             MapPin("bogota", LatLng(-4.7110, -74.0721)),
-            MapPin("lima", LatLng(-12.0464, -77.0428)),
+            MapPin("lima", LatLng(4-12.0464, -77.0428)),
             MapPin("salvador", LatLng(-12.9777, -38.5016))
         )
 
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding)) {
-                        CustomMapView(Modifier.matchParentSize(), list)
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .background(Color.White)
+                    ) {
+                        CustomMapView(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(20.dp),
+                            list
+                        )
                     }
                 }
             }
--- app/src/main/java/com/map/sampleapp/ui/CustomMapView.kt
+++ app/src/main/java/com/map/sampleapp/ui/CustomMapView.kt
@@ -2,11 +2,20 @@ package com.map.sampleapp.ui
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLngBounds
@@ -66,18 +75,20 @@ fun CustomMapView(modifier: Modifier, list: List<MapPin>) {
                 state = MarkerState(position = mapPin.coordinate),
                 icon = context.bitmapDescriptorFromVector(R.drawable.ico_pin),
                 onInfoWindowClick = {
-                    //onClickWorker(worker.id)
+                    Toast.makeText(context, "Clicked on " + mapPin.name, Toast.LENGTH_SHORT).show()
                 }
             ) {
                 if (it.isInfoWindowShown)
                     markerWithInfoWindow = it
-//                                    WorkerWidget(
-//                                        modifier = Modifier
-//                                            .defaultMinSize(minWidth = 200.dp)
-//                                            .background(Color.White, RoundedCornerShape(10)),
-//                                        worker = worker,
-//                                        showLocationUpdated = true
-//                                    ) {}
+                Text(
+                    modifier = Modifier
+                        .background(Color.White, RoundedCornerShape(10))
+                        .padding(8.dp),
+                    fontWeight = FontWeight.Normal,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.DarkGray,
+                    text = mapPin.name
+                )
             }
         }
     }
